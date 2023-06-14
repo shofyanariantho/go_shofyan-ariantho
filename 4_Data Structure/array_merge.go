bee7d413730dda82c669b4f4e214b1084ff5e7dd
shofyanariantho <shofyanariantho@gmail.com>
2023-06-14 16:35:15 +0700

    update question

---
+++ 4_Data Structure/array_merge.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func ArrayMerge(arrayA, arrayB []string) []string {
+
+}
+
+func main(){
+	fmt.Println(ArrayMerge([]string{"king", "devil jin", "akuma"}, []string{"eddie", "steve", "geese"}))
+	fmt.Println(ArrayMerge([]string{"sergei", "jin"}, []string{"jin", "steve", "bryan"}))
+	fmt.Println(ArrayMerge([]string{"alisa", "yoshimitsu"}, []string{"devil jin", "yoshimitsu", "alisa"}))
+}
